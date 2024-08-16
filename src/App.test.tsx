c4007dc1b8557c2954c303c4f39445228e21e687
Ilnur Gareev <GareevIIld@tatneft.tatar>
2024-08-16 12:05:52 +0300

    feature:setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,162 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+const setup = () => {
+  const input = screen.getByPlaceholderText('What needs to be done?');
+  const form = screen.getByTestId('form');
+  const todoList = screen.getByTestId('todo_list');
+
+  return { input, form, todoList}
+}
+
+const getFilterButton = () => {
+  const filterToActiveButton = screen.getByTestId('filter_to_active');
+  const filterToCompletedButton = screen.getByTestId('filter_to_completed');
+
+  return { filterToActiveButton, filterToCompletedButton }
+}
+
+const submitForm = (input: HTMLElement, form: HTMLElement) => {
+  fireEvent.change(input, { target: { value: 'todo1' } });
+  fireEvent.submit(form);
+
+  return screen.getByLabelText('active_todo');
+}
+
+const completeTodo = () => {
+  const completedButton = screen.getByLabelText('completed_todo_button');
+  fireEvent.click(completedButton);
+
+  return screen.getByLabelText('completed_todo');
+}
+
+test('renders todos', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const todoElement = screen.getByText(/todos/i);
+
+  expect(todoElement).toBeInTheDocument();
+});
+
+test('write new todo', () => {
+  render(<App />);
+  const { input } = setup();
+
+  fireEvent.change(input, { target: { value: 'todo1' } });
+
+  expect(input).toHaveValue('todo1');
+});
+
+test('clear new todo text', () => {
+  render(<App />);
+  const { input } = setup();
+
+  fireEvent.change(input, { target: { value: 'todo1' } });
+
+  const button = screen.getByTestId('clear_text');
+  fireEvent.click(button);
+
+  expect(input).toHaveValue('');
+});
+
+test('clear input after submit', () => {
+  render(<App />);
+  const { input, form } = setup();
+
+  fireEvent.submit(form);
+
+  expect(input).toHaveValue('');
+});
+
+test('add todo', () => {
+  render(<App />);
+  const { input, form, todoList } = setup();
+
+  const new_todo = submitForm(input, form);
+
+  const count_active_todo = screen.getByTestId('count_active_todo');
+
+  expect(count_active_todo).toHaveTextContent('1');
+  expect(todoList).toContainElement(new_todo);
+});
+
+test('delete todo', () => {
+  render(<App />);
+  const { input, form, todoList } = setup();
+
+  const new_todo = submitForm(input, form)
+
+  const deleteButton = screen.getByLabelText('delete_todo');
+  fireEvent.click(deleteButton);
+
+  const count_active_todo = screen.getByTestId('count_active_todo');
+
+  expect(count_active_todo).toHaveTextContent('0');
+  expect(todoList).not.toContainElement(new_todo);
+});
+
+test('completed todo', () => {
+  render(<App />);
+  const { input, form, todoList } = setup();
+
+  submitForm(input, form)
+
+  const completed_todo = completeTodo();
+  const completed_svg = screen.getByLabelText('completed_svg');
+  const count_active_todo = screen.getByTestId('count_active_todo');
+
+  expect(count_active_todo).toHaveTextContent('0');
+  expect(todoList).toContainElement(completed_todo);
+  expect(todoList).toContainElement(completed_svg);
+});
+
+test('clear completed todo', () => {
+  render(<App />);
+  const { input, form, todoList } = setup();
+
+  submitForm(input, form)
+
+  const completed_todo = completeTodo();
+  const clear_completed_todos_button = screen.getByTestId('clear_completed_todos');
+
+  fireEvent.click(clear_completed_todos_button);
+
+  expect(todoList).not.toContainElement(completed_todo);
+});
+
+test('filter to active todo', () => {
+  render(<App />);
+  const { input, form, todoList } = setup();
+
+  const active_todo = submitForm(input, form)
+
+  const { filterToActiveButton, filterToCompletedButton } = getFilterButton()
+
+  expect(todoList).toContainElement(active_todo);
+
+  fireEvent.click(filterToActiveButton);
+  expect(todoList).toContainElement(active_todo);
+
+  fireEvent.click(filterToCompletedButton);
+  expect(todoList).not.toContainElement(active_todo);
+});
+
+test('filter to completed todo', () => {
+  render(<App />);
+  const { input, form, todoList } = setup();
+
+  submitForm(input, form)
+
+  const completed_todo = completeTodo();
+  const { filterToActiveButton, filterToCompletedButton } = getFilterButton()
+
+  expect(todoList).toContainElement(completed_todo);
+
+  fireEvent.click(filterToActiveButton);
+  expect(todoList).not.toContainElement(completed_todo);
+
+  // todo: i dont know why it isnt work
+  // fireEvent.click(filterToCompletedButton);
+  // expect(todoList).toContainElement(completed_todo);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useState } from 'react';
+import classes from './App.module.scss';
+import { ChevronDownIcon, CircleCheckBigIcon, CircleIcon, XIcon } from 'lucide-react';
+import { toast } from 'react-toastify';
+
+type TTodo = {
+  id: number;
+  text: string;
+  type: 'active' | 'completed';
+};
+type TFilterTypes = 'active' | 'completed' | 'all';
+
+const filterButtons: { type: TFilterTypes; text: string }[] = [
+  { type: 'all', text: 'All' },
+  { type: 'active', text: 'Active' },
+  { type: 'completed', text: 'Completed' },
+];
 
 function App() {
+  const [todos, setTodos] = useState<TTodo[]>([]);
+  const [newTodoText, setNewTodoText] = useState('');
+  const [filterType, setFilterType] = useState<TFilterTypes>('all');
+
+  const addNewTodo = (e: FormEvent) => {
+    e.preventDefault();
+    if (!newTodoText) {
+      toast.error('Нет текста задания');
+      return;
+    }
+    setNewTodoText('');
+    setTodos((old) => [...old, { id: old.length + 1, text: newTodoText, type: 'active' }]);
+    toast.success('Задание успешно добавлено');
+  };
+
+  const deleteTodo = (todoId: number) => {
+    setTodos((old) => old.filter((old_todo) => old_todo.id !== todoId));
+    toast.success('Задание успешно удалено');
+  };
+
+  const completedTodo = (todoId: number) => {
+    setTodos((old) => old.map((old_todo) => (old_todo.id === todoId ? { ...old_todo, type: 'completed' } : old_todo)));
+    toast.success('Задание успешно выполнена');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <h1>todos</h1>
+      <div>
+        <form onSubmit={addNewTodo} data-testid='form'>
+          <ChevronDownIcon />
+          <input
+            value={newTodoText}
+            onChange={(e) => setNewTodoText(e.target.value)}
+            placeholder='What needs to be done?'
+          />
+          <button type='button' data-testid='clear_text' onClick={(e) => setNewTodoText('')}>
+            <XIcon />
+          </button>
+        </form>
+        <div className={classes.todosList} data-testid='todo_list'>
+          {todos
+            .filter((todo) => (filterType !== 'all' ? todo.type === filterType : true))
+            .map((todo) => (
+              <div
+                key={todo.id}
+                className={classes.todosList_item}
+                aria-label={todo.type === 'active' ? 'active_todo' : 'completed_todo'}
+              >
+                {todo.type === 'active' ? (
+                  <button aria-label='completed_todo_button' onClick={() => completedTodo(todo.id)}>
+                    <CircleIcon />
+                  </button>
+                ) : (
+                  <CircleCheckBigIcon color='green' aria-label='completed_svg' />
+                )}
+                <span className={todo.type === 'completed' ? classes.todosList_item__completed : ''}>{todo.text}</span>
+                <button onClick={() => deleteTodo(todo.id)} aria-label='delete_todo'>
+                  <XIcon />
+                </button>
+              </div>
+            ))}
+        </div>
+        <div className={classes.actions}>
+          <small>
+            <span data-testid='count_active_todo'>{todos.filter((todo) => todo.type === 'active').length}</span> items
+            left
+          </small>
+          <div className={classes.actions_filters}>
+            {filterButtons.map((item) => (
+              <button
+                data-testid={`filter_to_${item.type}`}
+                className={item.type === filterType ? classes.active : ''}
+                onClick={() => setFilterType(item.type)}
+                key={item.type}
+              >
+                {item.text}
+              </button>
+            ))}
+          </div>
+          <button
+            data-testid='clear_completed_todos'
+            onClick={() => setTodos((old) => old.filter((todo) => todo.type !== 'completed'))}
+          >
+            Clear completed
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
